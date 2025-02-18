2c2de844b06c7f3ecb56297a2dded5c4093e0b49
alicetindra <tindra.lindgren@gmail.com>
2025-02-18 17:04:42 +0100

    Created unit tests for Dice and Die

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,51 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.ntnu.idatt2003.boardgame.Dice;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DiceTest {
+
+  private Dice dice;
+
+  @BeforeEach
+  public void setUp() {
+    dice = new Dice(6);
+  }
+
+  //------------------------Positive tests--------------------------------
+
+  @Test
+  public void testRoll() {
+    int result = dice.roll();
+    assertTrue(result >= 5 && result <= 30, "Total roll result should be between 5 and 30");
+  }
+
+  @Test
+  public void testGetDie() {
+    dice.roll();
+    for (int i = 0; i < 5; i++) {
+      int result = dice.getDie(i);
+      assertTrue(result >= 1 && result <= 6, "Die value should be between 1 and 6");
+    }
+  }
+
+  //--------------------------Negative tests-------------------------
+
+  @Test
+  public void testGetDieInvalidIndex() {
+    Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+      dice.getDie(8); // Index out of bounds
+    });
+    assertEquals("Index out of bounds", exception.getMessage());
+  }
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/DieTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.ntnu.idatt2003.boardgame.Die;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DieTest {
+  private Die die;
+
+  @BeforeEach
+  public void setUp() {
+    die = new Die();
+  }
+
+  @Test
+  public void testRoll() {
+    int result = die.roll();
+    assertTrue(result >= 1 && result <= 6, "Roll result should be between 1 and 6");
+  }
+
+  @Test
+  public void testGetValue() {
+    die.roll();
+    int result = die.getValue();
+    assertTrue(result >= 1 && result <= 6, "Value should be between 1 and 6");
+  }
+}
