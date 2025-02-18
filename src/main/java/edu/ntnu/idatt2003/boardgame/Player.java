83175531d3c41de0acf1fef98afdec6b951cba0f
Nicoline Lean Tønnessen <nicolinetonne@gmail.com>
2025-02-18 14:02:36 +0100

    Empty Player class

---
+++ src/main/java/edu/ntnu/idatt2003/boardgame/Player.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2003.boardgame;
+
+public class Player {
+    String name;
+    Tile currentTile;
+
+    public Player(String name, BoardGame game) {
+        this.name = name;
+        this.game = game;
+    }
+    public void placeOnTile(Tile tile){
+        
+    }
+    public void move(int steps){
+        
+    }
+}
