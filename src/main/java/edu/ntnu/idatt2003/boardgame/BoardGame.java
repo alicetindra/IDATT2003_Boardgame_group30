8eea90fd22a06a35b996b9f72e488e008959afb7
Nicoline Lean Tønnessen <nicolinetonne@gmail.com>
2025-02-18 14:23:06 +0100

    Created BoardGame class

---
+++ src/main/java/edu/ntnu/idatt2003/boardgame/BoardGame.java
@@ -0,0 +1,26 @@
+package edu.ntnu.idatt2003.boardgame;
+
+import java.util.List;
+
+public class BoardGame {
+    Board board;
+    Player currentPlayer;
+    List<Player> players;
+    Dice dice;
+
+   public void addPlayer(Player player) {
+       players.add(player);
+   }
+   public void createBoard(){
+       board = new Board();
+   }
+   public void createDice(int numberOfDice){
+       dice = new Dice(numberOfDice);
+   }
+   public void Play(){
+
+   }
+   public Player getWinner(){
+       return null;
+   }
+}
