0df0be781b03718098a3b29418956b71ea349993
Nicoline Lean Tønnessen <nicolinetonne@gmail.com>
2025-02-18 14:25:40 +0100

    Created LadderAction class

---
+++ src/main/java/edu/ntnu/idatt2003/boardgame/LadderAction.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2003.boardgame;
+
+public class LadderAction implements TileAction {
+    LadderAction(int destinationTile, String description) {
+
+    }
+
+    @Override
+    public void perform(Player player) {
+
+    }
+}
