ea8754556560122285121d6501203923ab5a3a02
Nicoline Lean Tønnessen <nicolinetonne@gmail.com>
2025-02-21 14:55:41 +0100

    Empty BoardGameTest

---
+++ src/test/java/BoardGameTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.ntnu.idatt2003.boardgame.BoardGame;
+import edu.ntnu.idatt2003.boardgame.Board;
+import edu.ntnu.idatt2003.boardgame.Tile;
+import edu.ntnu.idatt2003.boardgame.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class BoardGameTest {
+
+}
