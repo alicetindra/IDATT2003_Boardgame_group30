d1a7d29809904aa8b26267cf32027de6e516666a
alicetindra <tindra.lindgren@gmail.com>
2025-02-21 12:16:22 +0100

    Created exceptions for player constructor

--- src/main/java/edu/ntnu/idatt2003/boardgame/Player.java
+++ src/main/java/edu/ntnu/idatt2003/boardgame/Player.java
@@ -3,15 +3,21 @@ package edu.ntnu.idatt2003.boardgame;
 public class Player {
 
     private final BoardGame game;
-    private String name;
+    private final String name;
     private Tile currentTile;
 
     public Player(String name, BoardGame game) {
+        if(name == null || name.isEmpty()){
+            throw new IllegalArgumentException("Player name cannot be null or empty");
+        }
+        if(game == null){
+            throw new IllegalArgumentException("BoardGame cannot be null");
+        }
         this.name = name;
         this.game = game;
     }
     public void placeOnTile(Tile tile){
-        
+
     }
     public void move(int steps){
         
